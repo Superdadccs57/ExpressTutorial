51dc3fe0fc0aac0a76a82dbe400e269e4408ca53
Superdadccs57 <crazytrain401@hotmail.com>
2021-11-13 16:40:09 -0700

    Params-QueryString

---
+++ node-express-course/02-express-tutorial/8-Params.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express()
+const {products} = require('./data')
+
+app.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
+})
+
+app.get('/api/products', (req, res) =>{
+    const newProducts = products.map((product)=>{
+        const {id,name,image} = product;
+        return {id,name,image}
+    })
+    res.json(newProducts)
+})
+app.get('/api/products/:productID', (req, res) =>{
+    // console.log(req);
+    // console.log(req.params)
+    const {productID} = req.params;
+
+    const singleProduct = products.find((product)=> product.id === Number(productID))
+    if(!singleProduct) {
+        return res.status(404).send('Product does not exist')
+    }
+    res.json(singleProduct)
+})
+
+// More Info
+app.get('/api/products/:productID/reviews/:reviewID', (req, res)=>{
+    console.log(req.params)
+    res.send('Hello World')
+})
+
+app.listen(5000, ()=>{
+    console.log('Server is Listening to port 5000...')
+})
