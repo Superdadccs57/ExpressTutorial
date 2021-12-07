bf6c4e04f804038a20ff790189a89f1cba14c851
Superdadccs57 <crazytrain401@hotmail.com>
2021-12-07 09:58:47 -0700

    POSTMAN PUT METHOD adding new PUT file

---
+++ node-express-course/02-express-tutorial/16-Methods-PUT.js
@@ -0,0 +1,69 @@
+// Global Variables
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+let { people } = require('./data')
+
+// static assets
+app.use(express.static('./methods-public'))
+
+// PARSE form data
+app.use(express.urlencoded({extended: false}))
+
+// PARSE JSON
+app.use(express.json())   
+
+app.get('/api/people', (req, res) => {
+    res.status(200).json({success: true, data: people})
+})
+
+app.post('/api/people', (req, res) => {
+    const {name} = req.body;
+    if(!name){
+        return res.status(400).json({success: false, msg: 'please provide name value'})
+    }
+    res.status(201).json({success: true,person:name})
+})
+
+app.post('/api/postman/people', (req, res) => {
+    const { name } = req.body;
+    if(!name) {
+        return res.status(400).json({success: false, msg:'please provide name value'})
+    } else {
+        res.status(201).send({success: true, data: [...people, name]})
+    }
+})
+
+app.post('/login', (req, res) =>{
+    const {name} = req.body;
+    if(name){
+        return res.status(200).send(`Welcome ${name}`)
+    } else {
+
+        res.status(200).send('Please Enter Credentials')
+    }
+})
+
+app.put('/api/people/:id',(req, res)=> {
+    const {id} = req.params
+    const {name} = req.body
+    
+    const person = people.find((person)=>person.id === Number(id))
+    if(!person) {
+        return res
+            .status(404)
+            .json({success: false, msg:`No person with id ${id}`})
+    } 
+    const newPeople = people.map((person) => {
+        if(person.id === Number(id)) {
+            person.name = name
+        }
+        return person
+    })
+    res.status(200).json({success: true, data: newPeople})
+})
+
+
+app.listen(port, () =>{
+    console.log(`Listening on port ${port}.....`)
+})
