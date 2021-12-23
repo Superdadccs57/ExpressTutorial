f04357f75e9c90ef3f34c26971edd20deb479d94
Superdadccs57 <crazytrain401@hotmail.com>
2021-12-22 20:51:07 -0700

    Express Router Update

---
+++ node-express-course/02-express-tutorial/app.js
@@ -0,0 +1,26 @@
+// Global Variables
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const people = require('./routes/people')
+const auth = require('./routes/auth')
+
+
+// static assets
+app.use(express.static('./methods-public'))
+
+// PARSE form data
+app.use(express.urlencoded({extended: false}))
+
+// PARSE JSON
+app.use(express.json())
+app.use('/api/people', people)
+app.use('/login', auth)  
+
+
+
+
+
+app.listen(port, () =>{
+    console.log(`Listening on port ${port}.....`)
+})
