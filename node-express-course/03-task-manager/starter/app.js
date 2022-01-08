bab5139a486ef16a2e7c62f3f7be312960b493c9
Superdadccs57 <crazytrain401@hotmail.com>
2022-01-08 15:42:03 -0700

    setting up controllers and models

--- node-express-course/03-task-manager/starter/app.js
+++ node-express-course/03-task-manager/starter/app.js
@@ -1,16 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const tasks = require('./routes/tasks')
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require('dotenv').config();
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 // Routes
 
-app.get('/Hello', (req,res) => {
-    res.send('Task Manager App')
-})
-app.use('/api/v1/tasks',tasks)
+app.get("/Hello", (req, res) => {
+  res.send("Task Manager App");
+});
+app.use("/api/v1/tasks", tasks);
 
 //app.get('/api/v1/tasks')          - get all the tasks
 //app.post('/api/v1/tasks')         - create a new task
@@ -19,7 +21,17 @@ app.use('/api/v1/tasks',tasks)
 //app.delete('/api/v1/tasks:id')    - delete task
 
 // Server Setup
-const port = process.env.PORT || 5000;
-app.listen(port, ()=>{
-    console.log(`Listening to ${port}`);
-});
+const port = 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Listening to ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
