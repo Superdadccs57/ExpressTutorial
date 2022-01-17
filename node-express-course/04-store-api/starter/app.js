90071ac280911116870bb5641d825f9cc443049b
Superdadccs57 <crazytrain401@hotmail.com>
2022-01-17 14:24:41 -0700

    Store API complete

--- node-express-course/04-store-api/starter/app.js
+++ node-express-course/04-store-api/starter/app.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 // async errors
-
+require('express-async-errors')
 
 const express = require('express');
 const app = express();
-
+const connectDB = require('./db/connect')
+const productRouter = require('./routes/products')
 const notFoundMiddleware = require("./middleware/not-found")
 const errorMiddleware = require("./middleware/error-handler");
 
@@ -15,6 +16,7 @@ app.use(express.json())
 app.get('/', (req,res) => {
     res.send(`<h1>Store API</h1><a href="/api/v1/products">products route</a>`)
 })
+app.use('/api/v1/products', productRouter)
 
 // products route
 app.use(notFoundMiddleware)
@@ -25,9 +27,10 @@ const port = process.env.PORT || 5000
 const start = async () => {
     try {
         // connectDB
-        app.listen(port, console.log(`Server is listening to ${port}.....`))
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => console.log(`Server is listening to ${port}.....`))
     } catch (error) {
         console.log(error)
     }
 }
-start()
+start();
---
+++ node-express-course/04-store-api/starter/controllers/products.js
@@ -0,0 +1,76 @@
+const Product = require('../models/product')
+
+const getAllProductsStatic = async (req,res) => {
+    
+    const products = await Product.find({price:{$gt:30}})
+    .sort('price')
+    .limit(10)
+    .select(' name price')
+    .skip(1)
+    res.status(200).json({nbHits: products.length, products })
+}
+
+const getAllProducts = async (req,res) =>{
+    const {featured, company, name, sort, fields, numericFilters} = req.query
+    const queryObject = {}
+    if (featured){
+        queryObject.featured = featured === 'true'? true:false
+    }
+    if(company){
+        queryObject.company = company
+    }
+    if(name){
+        queryObject.name = { $regex: name, $options:'i'}
+    }
+    // Numeric Filters
+    // REMEMBER THAT THE BROWSER SEES OPERATORS DIFFERENTLY THAN MONGOOSE SO YOU NEED TO USE THE FOLLOWING CODE TO TRANSLATE SO TO SPEAK
+    if(numericFilters) {
+        const operatorMap = {
+            '>':'$gt',
+            '>=':'$gte',
+            '=':'$eq',
+            '<':'$lt',
+            '<':'$lte',
+        }
+        const regEx = /\b(>|>=|=|<|<=)\b/g
+
+        let filters = numericFilters.replace(regEx,(match)=>`-${operatorMap[match]}-`)
+        
+        const options = ['price','rating'];
+        filters = filters.split(',').forEach((item)=>{
+            const [field, operator,value] = item.split('-')
+            if(options.includes(field)){
+                queryObject[field] = {[operator]:Number(value)}
+            }
+        })
+    }
+
+
+    console.log(queryObject)
+    let result = Product.find(queryObject)
+    // Sort
+    if (sort){
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    } else {
+        result = result.sort("createdAt")
+    }
+    // fields
+    if (fields){
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)   
+    }
+    // pagination
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page -1) * limit
+
+    result = result.skip(skip).limit(limit)
+    const products = await result
+    res.status(200).json({ products, nbHits: products.length })
+}
+
+module.exports = {
+    getAllProducts,
+    getAllProductsStatic
+}
