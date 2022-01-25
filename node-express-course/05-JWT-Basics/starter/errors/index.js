042371ad1edbaa251ff48997f2110b8e16722d81
Superdadccs57 <crazytrain401@hotmail.com>
2022-01-25 14:26:09 -0700

    More Error Classes

---
+++ node-express-course/05-JWT-Basics/starter/errors/index.js
@@ -0,0 +1,5 @@
+const CustomAPIError = require('./custom-error')
+const badRequestError = require('./bad-request')
+const unAuthError = require('./unauthenticated')
+
+module.exports = {CustomAPIError,badRequestError,unAuthError}
