58e858dac4fd7f648caa0f4d4bef9a1cc27f777d
Superdadccs57 <crazytrain401@hotmail.com>
2022-01-31 14:28:51 -0700

    Controllers Basic Setup

---
+++ node-express-course/06-jobs-api/starter/controllers/jobs.js
@@ -0,0 +1,24 @@
+const getAllJobs = async (req, res) => {
+    res.send('get all jobs')
+}
+const getJob = async (req, res) => {
+    res.send('get a job')
+}
+const createJob = async (req, res) => {
+    res.send('create a Job')
+}
+const updateJob = async (req, res) => {
+    res.send('update a job')
+}
+const deleteJob = async (req, res) => {
+    res.send('delete a job')
+}
+
+
+module.exports = {
+    getAllJobs,
+    getJob,
+    createJob,
+    updateJob,
+    deleteJob,
+}
