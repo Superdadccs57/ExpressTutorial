58ee4020099101128c3b01ccd3ee1e5b4d4e6af9
Superdadccs57 <crazytrain401@hotmail.com>
2022-01-31 21:08:43 -0700

    Generate Token - Instance Method

--- node-express-course/06-jobs-api/starter/models/User.js
+++ node-express-course/06-jobs-api/starter/models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const UserSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -28,5 +30,9 @@ UserSchema.pre('save', async function(next){
     this.password = await bcrypt.hash(this.password,salt)
     next()
 })
+
+UserSchema.methods.createJWT = function() {
+    return jwt.sign({userID:this._id, name:this.name}, 'jwtSecret', {expiresIn: '30d'},)
+}
 
 module.exports = mongoose.model('User', UserSchema)
