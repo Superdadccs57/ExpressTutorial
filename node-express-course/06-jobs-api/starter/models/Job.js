44c8557c34c9ea3abca3d46e61b98a6002e61f33
Superdadccs57 <crazytrain401@hotmail.com>
2022-02-05 08:16:05 -0700

    Job Model

---
+++ node-express-course/06-jobs-api/starter/models/Job.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongooose')
+
+const JobSchema = new mongoose.Schema({
+    company:{
+        type:String,
+        required: [true, 'Please provide company name'],
+        maxlength:50
+    },
+    position:{
+        type:String,
+        required: [true, 'Please provide company name'],
+        maxlength:100,
+    },
+    Status: {
+        type:String,
+        enum:['interview', 'declined','pending'],
+        default: 'pending',
+    },
+    createdBy:{
+        type:mongoose.Types.ObjectId,
+        ref:'User',
+        required: [true, 'Please provide user']
+    }
+},{timestamps:true})
+
+module.exports = mongoose.model('Job', JobSchema)
