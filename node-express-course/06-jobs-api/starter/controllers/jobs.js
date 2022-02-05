23a07007096b77b928a74fbe461086b443f43d29
Superdadccs57 <crazytrain401@hotmail.com>
2022-02-05 09:33:22 -0700

    Get All Jobs

--- node-express-course/06-jobs-api/starter/controllers/jobs.js
+++ node-express-course/06-jobs-api/starter/controllers/jobs.js
@@ -1,30 +1,30 @@
-const Job = require('../models/Job')
-const { StatusCodes } = require('http-status-codes')
-const { NotFoundError, BadRequestError } = require('../errors')
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { NotFoundError, BadRequestError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
-    res.send('get all jobs')
-}
+  const jobs = await Job.find({ createdBy: req.user.userID }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
+};
 const getJob = async (req, res) => {
-    res.send('get a job')
-}
+  res.send("get a job");
+};
 const createJob = async (req, res) => {
-    req.body.createdBy = req.user.userID
-    const job = await Job.create(req.body)
-    res.status(StatusCodes.CREATED).json(job)
-}
+  req.body.createdBy = req.user.userID;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json(job);
+};
 const updateJob = async (req, res) => {
-    res.send('update a job')
-}
+  res.send("update a job");
+};
 const deleteJob = async (req, res) => {
-    res.send('delete a job')
-}
-
+  res.send("delete a job");
+};
 
 module.exports = {
-    getAllJobs,
-    getJob,
-    createJob,
-    updateJob,
-    deleteJob,
-}
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+};
