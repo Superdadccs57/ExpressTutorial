1558a83399d32676f29de5a6ca1157ec03c3031d
Superdadccs57 <crazytrain401@hotmail.com>
2022-02-06 15:24:35 -0700

    Product Model Setup

---
+++ node-express-course/07-file-upload/starter/models/Product.js
@@ -0,0 +1,15 @@
+const { string } = require('joi');
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
