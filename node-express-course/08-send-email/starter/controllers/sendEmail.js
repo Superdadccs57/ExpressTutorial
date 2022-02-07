bc0f515b2a2c33ce7387185dce4721b29760f9b1
Superdadccs57 <crazytrain401@hotmail.com>
2022-02-07 11:16:37 -0700

    Sending Email with SendGrid

--- node-express-course/08-send-email/starter/controllers/sendEmail.js
+++ node-express-course/08-send-email/starter/controllers/sendEmail.js
@@ -1,26 +1,39 @@
 const nodemailer = require('nodemailer');
+const sendGrid = require('@sendgrid/mail');
+
+// const sendEmailEthereal = async (req, res) => {
+//   let testAccount = await nodemailer.createTestAccount();
+
+//   const transporter = nodemailer.createTransport({
+//     host: 'smtp.ethereal.email',
+//     port: 587,
+//     secure: false, //true for port 465, false for other ports
+//     auth: {
+//       user: 'jadyn.moore33@ethereal.email',
+//       pass: 'wkMgtZNG6aKrGnCW8H',
+//     },
+//   });
+
+//   let info = await transporter.sendMail({
+//     from: '"Thomas Fentie" <fentiewebdevelopment@shaw.ca>',
+//     to: 'crazytrain401@hotmail.com',
+//     subject: 'Hello Test',
+//     text: 'Hello World?',
+//     html: '<h1>Hello World!</h1>',
+//   });
+// res.json(info);
+// };
 
 const sendEmail = async (req, res) => {
-  let testAccount = await nodemailer.createTestAccount();
-
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    secure: false, //true for port 465, false for other ports
-    auth: {
-      user: 'jadyn.moore33@ethereal.email',
-      pass: 'wkMgtZNG6aKrGnCW8H',
-    },
-  });
-
-  let info = await transporter.sendMail({
-    from: '"Thomas Fentie" <fentiewebdevelopment@shaw.ca>',
+  sendGrid.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
     to: 'crazytrain401@hotmail.com',
+    from: 'fentiewebdevelopment@shaw.ca',
     subject: 'Hello Test',
     text: 'Hello World?',
     html: '<h1>Hello World!</h1>',
-  });
-
+  };
+  const info = await sendGrid.send(msg);
   res.json(info);
 };
 
