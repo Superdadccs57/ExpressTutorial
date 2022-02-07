56773679a0bf6c17b53e4518fe074d72465d125e
Superdadccs57 <crazytrain401@hotmail.com>
2022-02-06 17:56:09 -0700

    Upload to Cloudinary

--- node-express-course/07-file-upload/starter/controllers/uploadsController.js
+++ node-express-course/07-file-upload/starter/controllers/uploadsController.js
@@ -1,30 +1,47 @@
-const path = require('path')
-const { statusCodes, StatusCodes } = require('http-status-codes')
-const CustomError = require('../errors')
+const path = require('path');
+const { statusCodes, StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+const cloudinary = require('cloudinary').v2;
+
+const uploadProductImageLocal = async (req, res) => {
+  //check if file exists
+  //check format
+  //check size
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No File Uploaded');
+  }
+  const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please Upload Image');
+  }
+  const maxSize = 1024 * 1024;
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError('Image is to large smaller than 1kb');
+  }
+
+  const imagePath = path.join(
+    __dirname,
+    '../public/uploads/' + `${productImage.name}`
+  );
+
+  await productImage.mv(imagePath);
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
+};
 
 const uploadProductImage = async (req, res) => {
-    //check if file exists
-    //check format
-    //check size
-    if (!req.files) {
-        throw new CustomError.BadRequestError('No File Uploaded')
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    {
+      use_filename: true,
+      folder: 'file_upload',
     }
-    const productImage = req.files.image;
-
-    if (!productImage.mimetype.startsWith('image')) {
-        throw new CustomError.BadRequestError('Please Upload Image')
-    }
-    const maxSize = 1024 * 1024
-    if (productImage.size > maxSize) {
-        throw new CustomError.BadRequestError('Image is to large smaller than 1kb')
-    }
-
-    const imagePath = path.join(__dirname, '../public/uploads/' + `${productImage.name}`)
-    
-    await productImage.mv(imagePath)
-    return res.status(StatusCodes.OK).json({image:{src:`/uploads/${productImage.name}`}})
-}
+  );
+  return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
+};
 
 module.exports = {
-    uploadProductImage
-}
+  uploadProductImage,
+};
