5f6a0179c251fef19a0bbc5f7574b4f591b7c43f
Superdadccs57 <crazytrain401@hotmail.com>
2022-02-07 13:45:52 -0700

    API Keys and flow overview

--- node-express-course/09-stripe-payment/starter/public/browser-app.js
+++ node-express-course/09-stripe-payment/starter/public/browser-app.js
@@ -6,7 +6,7 @@ const total_amount = 10998;
 const shipping_fee = 1099;
 
 var stripe = Stripe(
-  'pk_test_51I87djFp5pnuKUXgBVIHiR36vVAWyfuyb7ckrhgyDNA1kM0GWHas9ZGUAgwJSFNUxrbyE6NwlMNmls1iGSfzHDdE00DQB3y6AH'
+  'pk_test_51KQdAMC1VmKrH3kewaYR2aZ23fBi7L8mVpJs3jzAJPbdhJMt7p1bI0swhWO0wJdbboHkbmanMbqJX9ac0jN9Et7z00LgUGQJ46'
 );
 
 // The items the customer wants to buy
