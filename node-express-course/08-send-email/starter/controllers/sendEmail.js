0935a568e2358d813ca1549444856d6a0a8fc13f
Superdadccs57 <crazytrain401@hotmail.com>
2022-02-07 09:25:24 -0700

    SendEmail with Ethereal

--- node-express-course/08-send-email/starter/controllers/sendEmail.js
+++ node-express-course/08-send-email/starter/controllers/sendEmail.js
@@ -1,5 +1,27 @@
-const sendEmail = async (req, res) => {
-    res.send('send email func')
-}
+const nodemailer = require('nodemailer');
 
-module.exports = sendEmail
+const sendEmail = async (req, res) => {
+  let testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false, //true for port 465, false for other ports
+    auth: {
+      user: 'jadyn.moore33@ethereal.email',
+      pass: 'wkMgtZNG6aKrGnCW8H',
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"Thomas Fentie" <fentiewebdevelopment@shaw.ca>',
+    to: 'crazytrain401@hotmail.com',
+    subject: 'Hello Test',
+    text: 'Hello World?',
+    html: '<h1>Hello World!</h1>',
+  });
+
+  res.json(info);
+};
+
+module.exports = sendEmail;
