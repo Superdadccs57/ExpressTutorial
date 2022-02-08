f55a11d4a3668b26fdd5c142a6ff4c0585fbccc6
Superdadccs57 <crazytrain401@hotmail.com>
2022-02-08 13:22:57 -0700

    Created User, Hashed Password, and looked for existing email

--- node-express-course/10-e-commerce-api/starter/controllers/authController.js
+++ node-express-course/10-e-commerce-api/starter/controllers/authController.js
@@ -1,5 +1,20 @@
-const registerController = async (req,res) => {
-    res.send("Register User")
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+const bcrypt = require('bcryptjs');
+
+const registerController = async (req, res) => {
+    const { name, email, password } = req.body
+    const emailAlreadyExists = await User.findOne({ email })
+    if (emailAlreadyExists) {
+        throw new CustomError.BadRequestError('Email already exists')
+    }
+    const salt = await bcrypt.genSalt(16);
+    const hashPassword = await bcrypt.hash(password,salt)
+    const tempUser = {name,email,password:hashPassword}
+
+    const user = await User.create({ ...tempUser });
+    res.status(StatusCodes.CREATED).json({ user });
 }
 
 const loginController = async (req, res) => {
