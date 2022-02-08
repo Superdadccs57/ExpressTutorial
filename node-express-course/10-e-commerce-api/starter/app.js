51a34e13fe92b07acc73a1fe2df8ee0acdc9100a
Superdadccs57 <crazytrain401@hotmail.com>
2022-02-08 10:04:24 -0700

    Basic Routes and Middleware and Morgan Package

--- node-express-course/10-e-commerce-api/starter/app.js
+++ node-express-course/10-e-commerce-api/starter/app.js
@@ -1,23 +1,45 @@
 // Dot ENV
-require('dotenv').config()
+require('dotenv').config();
+
+// Express Async Errors
+require('express-async-errors');
 
 // Express
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+// Rest of Packages
+const morgan = require('morgan')
 
 // Database
-const connectDB = require('./db/connect')
+const connectDB = require('./db/connect');
+
+// Middleware
+const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found');
+
+app.use(morgan('tiny'))
+app.use(express.json());
+
+//Routes
+app.get('/', (req, res) => {
+  res.send('eCommerce Project');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
 
 // Server
 const port = process.env.PORT || 5000;
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URL);
-        app.listen(port, () => {
-            console.log(`Server is listening on port ${port}...`)
-        })
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 start();
